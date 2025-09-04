c4ae3d8dd084c92c68351d37b139b45a0f58a695
mrlomax <mattylomax@googlemail.com>
2025-09-04 14:22:04 +0100

    Added alphabet for index and for loop

--- cipher.py
+++ cipher.py
@@ -1,3 +1,11 @@
-#number = 5
+#Caesar Cipher
 text = 'Hello World'
-print('\n'+text[0]+'\n')
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    index = alphabet.find(char)
+    new_index = index + shift
+    new_char = alphabet[new_index]
+    print('char:', char, 'new char:', new_char)
