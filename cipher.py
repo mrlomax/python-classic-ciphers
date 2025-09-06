c02a08faa27b123252969ac4e6235908aae7c858
mrlomax <mattylomax@googlemail.com>
2025-09-06 17:22:55 +0100

    Added Vigenere function with decryption functionality

--- cipher.py
+++ cipher.py
@@ -1,13 +1,13 @@
 #Caesar Cipher
 text = 'Hello Zaira'
 shift = 3
+custom_key = 'python'
 
 def caesar(message, offset):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
 
     for char in message.lower():
-
         if char == ' ':
             encrypted_text += char
         else:
@@ -18,5 +18,28 @@ def caesar(message, offset):
     print('plain text:', message)
     print('encrypted text:', encrypted_text) 
 
-caesar(text, shift)
-caesar(text, 13)
+
+def vigenere(message, key, direction=1):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+
+    for char in message.lower():
+        #Append space to the message
+        if char == ' ':
+            final_message += char
+        else:
+            #Find the right key character to encode/decode
+            key_char = key[key_index%len(key)]
+            key_index += 1
+            #Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction)%len(alphabet)
+            final_message += alphabet[new_index]
+    return final_message
+
+encryption = vigenere(text, custom_key, 1)
+print(encryption)
+decryption = vigenere(encryption, custom_key, -1)
+print(decryption)
